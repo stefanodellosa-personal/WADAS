5cde5c8c092135b14e13c47f99acdc21336e1a26
Alessandro Palla <alespalla.ap@gmail.com>
2024-10-16 07:35:57 +0200

    Implement review fix

--- src/domain/openvino_models.py
+++ src/domain/openvino_models.py
@@ -4,7 +4,10 @@ import numpy as np
 import openvino as ov
 import openvino.properties as props
 from PytorchWildlife.models import detection as pw_detection
+import logging
 import torch
+
+logger = logging.getLogger(__name__)
 
 core = ov.Core()
 core.set_property({props.cache_dir: "cache"})
@@ -12,8 +15,10 @@ core.set_property({props.cache_dir: "cache"})
 
 
 class DetectionModel:
+    """Class to load OpenVINO detection model"""
+
     def __init__(self, device="cpu") -> None:
-        print("Loading Openvino model")
+        logger.info("Loading Openvino model")
 
         ov_model = core.read_model("detection_model.onnx")
         self.model = ov.compile_model(ov_model, device_name=device.upper())
@@ -24,14 +29,17 @@ class DetectionModel:
 
 
 class OVMegaDetectorV5(pw_detection.MegaDetectorV5):
+    """Class to load OpenVINO MegaDetectorV5 model"""
+
     def __init__(self, device="cpu"):
         self.model = DetectionModel(device)
         self.device = "cpu"  # torch device, keep to CPU when using with OpenVINO
 
 
 class OVClassificationModel:
-    def __init__(self, weight_path, device):
+    """Class to load OpenVINO classification model"""
 
+    def __init__(self, weight_path, device):
         ov_model = core.read_model("classification_model.xml")
         self.model = ov.compile_model(ov_model, device_name=device.upper())
         self.device = "cpu"
