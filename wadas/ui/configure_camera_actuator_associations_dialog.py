f509e4cc6db495255255228c6a5a09dd367c6946
Stefano Dell'Osa <stefano.dellosa@gmail.com>
2024-11-05 15:13:37 +0100

    addressed PR comments

--- wadas/ui/configure_camera_actuator_associations_dialog.py
+++ wadas/ui/configure_camera_actuator_associations_dialog.py
@@ -6,8 +6,8 @@ from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon, QStandardItem, QStandardItemModel
 from PySide6.QtWidgets import QDialog
 
-from wadas.ui.actuators_management_dialog import DialogCameraActuatorManagement
 from wadas.domain.camera import Camera, cameras
+from wadas.ui.actuators_management_dialog import DialogCameraActuatorManagement
 from wadas.ui.qt.ui_configure_camera_to_actuator_associations import (
     Ui_DialogCameraActuatorAssociation,
 )
