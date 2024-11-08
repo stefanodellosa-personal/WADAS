15154bb31fab9d54012837ecfb8c1f1bdb43b24c
Alessandro Palla <alespalla.ap@gmail.com>
2024-11-08 10:08:58 +0100

    Use queque blocking feature to put the thread to sleep

--- wadas/domain/animal_detection_mode.py
+++ wadas/domain/animal_detection_mode.py
@@ -1,7 +1,7 @@
 """Animal Detection and Classification module."""
 
 import logging
-import time
+from queue import Empty
 
 from wadas.domain.camera import Camera, cameras, img_queue
 from wadas.domain.ftps_server import FTPsServer
@@ -49,7 +49,14 @@ class AnimalDetectionAndClassificationMode(OperationMode):
         # Run detection model
         while self.process_queue:
             self.check_for_termination_requests()
-            if not img_queue.empty():
+            # Get image from motion detection notification
+            # Timeout is set to 1 second to avoid blocking the thread
+            try:
+                cur_img = img_queue.get(timeout=1)
+            except Empty:
+                cur_img = None
+
+            if cur_img:
                 logger.debug("Processing image from motion detection notification...")
                 cur_img = img_queue.get()
                 results, detected_img_path = self.ai_model.process_image(cur_img["img"], True)
@@ -98,7 +105,7 @@ class AnimalDetectionAndClassificationMode(OperationMode):
                     # Send notification
                     if message and processed_img_path:
                         self.send_notification(processed_img_path, message)
-            time.sleep(1)
+
         self.execution_completed()
 
     def check_for_termination_requests(self):
