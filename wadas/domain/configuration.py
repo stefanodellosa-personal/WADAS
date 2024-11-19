c23bc1bd1279459cbe13f75cb01bf71e48761b66
cdimauro <cesare.di.mauro@gmail.com>
2024-11-19 20:56:52 +0100

    Refactor load/store config code

--- wadas/domain/configuration.py
+++ wadas/domain/configuration.py
@@ -22,23 +22,25 @@ from wadas.domain.usb_camera import USBCamera
 
 logger = logging.getLogger(__name__)
 
+_OPERATION_MODE_TYPE_VALUE_TO_TYPE = {mode.value: mode for mode in OperationMode.OperationModeTypes}
+
 
 def load_configuration_from_file(file_path):
-    """Method to load configuration from YAML file."""
+    """Load configuration from YAML file."""
 
-    with open(str(file_path), "r") as file:
-
+    with open(str(file_path)) as file_:
         logging.info("Loading configuration from file...")
-        wadas_config = yaml.safe_load(file)
 
+        wadas_config = yaml.safe_load(file_)
         # Applying configuration to WADAS from config file values
 
         # Notifiers
-        notification = wadas_config["notification"]
-        for key in notification:
-            if key in Notifier.notifiers:
-                if key == Notifier.NotifierTypes.EMAIL.value:
-                    Notifier.notifiers[key] = EmailNotifier(**notification[key])
+        Notifier.notifiers.update(
+            (key, EmailNotifier(**value))
+            for key, value in (wadas_config["notification"] or {}).items()
+            if key in Notifier.notifiers and key == Notifier.NotifierTypes.EMAIL.value
+        )
+
         # FTP Server
         if FTPsServer.ftps_server and FTPsServer.ftps_server.server:
             FTPsServer.ftps_server.server.close_all()
@@ -47,85 +49,82 @@ def load_configuration_from_file(file_path):
             if wadas_config["ftps_server"]
             else None
         )
+
         # Actuators
         Actuator.actuators.clear()
         for data in wadas_config["actuators"]:
-            if data["type"] == Actuator.ActuatorTypes.ROADSIGN.value:
-                actuator = RoadSignActuator.deserialize(data)
-                Actuator.actuators[actuator.id] = actuator
-            elif data["type"] == Actuator.ActuatorTypes.FEEDER.value:
-                actuator = FeederActuator.deserialize(data)
-                Actuator.actuators[actuator.id] = actuator
+            match data["type"]:
+                case Actuator.ActuatorTypes.ROADSIGN.value:
+                    actuator = RoadSignActuator.deserialize(data)
+                    Actuator.actuators[actuator.id] = actuator
+                case Actuator.ActuatorTypes.FEEDER.value:
+                    actuator = FeederActuator.deserialize(data)
+                    Actuator.actuators[actuator.id] = actuator
+
         # Camera(s)
         cameras.clear()
         for data in wadas_config["cameras"]:
-            if data["type"] == Camera.CameraTypes.USB_CAMERA.value:
-                usb_camera = USBCamera.deserialize(data)
-                cameras.append(usb_camera)
-            elif data["type"] == Camera.CameraTypes.FTP_CAMERA.value:
-                ftp_camera = FTPCamera.deserialize(data)
-                cameras.append(ftp_camera)
-                if FTPsServer.ftps_server:
-                    if not os.path.isdir(ftp_camera.ftp_folder):
-                        os.makedirs(ftp_camera.ftp_folder, exist_ok=True)
-                    credentials = keyring.get_credential(f"WADAS_FTP_camera_{ftp_camera.id}", "")
-                    if credentials:
-                        FTPsServer.ftps_server.add_user(
-                            credentials.username,
-                            credentials.password,
-                            ftp_camera.ftp_folder,
+            match data["type"]:
+                case Camera.CameraTypes.USB_CAMERA.value:
+                    usb_camera = USBCamera.deserialize(data)
+                    cameras.append(usb_camera)
+                case Camera.CameraTypes.FTP_CAMERA.value:
+                    ftp_camera = FTPCamera.deserialize(data)
+                    cameras.append(ftp_camera)
+                    if FTPsServer.ftps_server:
+                        if not os.path.isdir(ftp_camera.ftp_folder):
+                            os.makedirs(ftp_camera.ftp_folder, exist_ok=True)
+                        credentials = keyring.get_credential(
+                            f"WADAS_FTP_camera_{ftp_camera.id}", ""
                         )
+                        if credentials:
+                            FTPsServer.ftps_server.add_user(
+                                credentials.username,
+                                credentials.password,
+                                ftp_camera.ftp_folder,
+                            )
         Camera.detection_params = wadas_config["camera_detection_params"]
+
         # FastAPI Actuator Server
         FastAPIActuatorServer.actuator_server = (
             FastAPIActuatorServer.deserialize(wadas_config["actuator_server"])
             if wadas_config["actuator_server"]
             else None
         )
+
         # Ai model
         AiModel.detection_treshold = wadas_config["ai_model"]["ai_detect_treshold"]
         AiModel.classification_treshold = wadas_config["ai_model"]["ai_class_treshold"]
         AiModel.language = wadas_config["ai_model"]["ai_language"]
+
         # Operation Mode
-        if (
+        if operation_mode_type := _OPERATION_MODE_TYPE_VALUE_TO_TYPE.get(
             wadas_config["operation_mode"]
-            == OperationMode.OperationModeTypes.AnimalDetectionMode.value
         ):
-            OperationMode.cur_operation_mode_type = (
-                OperationMode.OperationModeTypes.AnimalDetectionMode
-            )
-        elif (
-            wadas_config["operation_mode"]
-            == OperationMode.OperationModeTypes.AnimalDetectionAndClassificationMode.value
-        ):
-            OperationMode.cur_operation_mode_type = (
-                OperationMode.OperationModeTypes.AnimalDetectionAndClassificationMode
-            )
-        elif wadas_config["operation_mode"] == OperationMode.OperationModeTypes.TestModelMode.value:
-            OperationMode.cur_operation_mode_type = OperationMode.OperationModeTypes.TestModelMode
+            OperationMode.cur_operation_mode_type = operation_mode_type
         else:
             OperationMode.cur_operation_mode = None
 
         logger.info("Configuration loaded from file %s.", file_path)
 
 
-def save_configuration_to_file(file):
-    """Method to save configuration to YAML file."""
+def save_configuration_to_file(file_):
+    """Save configuration to YAML file."""
 
     logger.info("Saving configuration to file...")
+
     # Prepare serialization for cameras per class type
-    cameras_to_dict = []
-    for camera in cameras:
-        if (
-            camera.type == Camera.CameraTypes.USB_CAMERA
-            or camera.type == Camera.CameraTypes.FTP_CAMERA
-        ):
-            cameras_to_dict.append(camera.serialize())
+    cameras_to_dict = [
+        camera.serialize()
+        for camera in cameras
+        if camera.type in (Camera.CameraTypes.FTP_CAMERA, Camera.CameraTypes.USB_CAMERA)
+    ]
+
     # Prepare serialization for notifiers per class type
-    notification = {}
-    for key, value in Notifier.notifiers.items():
-        if key and value:
-            notification[key] = Notifier.notifiers[key].serialize()
+    notification = {
+        key: value.serialize() for key, value in Notifier.notifiers.items() if key and value
+    }
+
     # Prepare serialization for actuators per class type
     actuators = [value.serialize() for key, value in Actuator.actuators.items() if key and value]
 
@@ -146,7 +145,7 @@ def save_configuration_to_file(file):
             if OperationMode.cur_operation_mode_type
             else ""
         ),
-        "ftps_server": (FTPsServer.ftps_server.serialize() if FTPsServer.ftps_server else ""),
+        "ftps_server": FTPsServer.ftps_server.serialize() if FTPsServer.ftps_server else "",
         "actuator_server": (
             FastAPIActuatorServer.actuator_server.serialize()
             if FastAPIActuatorServer.actuator_server
@@ -154,7 +153,7 @@ def save_configuration_to_file(file):
         ),
     }
 
-    with open(file, "w") as yamlfile:
-        yaml.safe_dump(data, yamlfile)
+    with open(file_, "w") as yaml_file:
+        yaml.safe_dump(data, yaml_file)
 
-    logger.info("Configuration saved to file %s.", file)
+    logger.info("Configuration saved to file %s.", file_)
