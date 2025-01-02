3c23cf5b1e907c8479efc4879d9b115e4e3ea651
Stefano Dell'Osa <stefano.dellosa@gmail.com>
2025-01-02 22:34:29 +0100

    License header introduced (#110)
    * Integrated license header in all python files
    
    TODO: complete for tests

--- wadas/domain/telegram_recipient.py
+++ wadas/domain/telegram_recipient.py
@@ -1,3 +1,23 @@
+# This file is part of WADAS project.
+#
+# WADAS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WADAS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WADAS. If not, see <https://www.gnu.org/licenses/>.
+#
+# Author(s): Stefano Dell'Osa, Alessandro Palla, Cesare Di Mauro, Antonio Farina
+# Date: 2024-12-23
+# Description: Module containing MainWindow class and methods.
+
+
 class TelegramRecipient:
 
     def __init__(self, recipient_id, name=None):
