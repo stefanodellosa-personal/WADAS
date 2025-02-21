3d07d57955321a3c3a6a0d9ecd19029f61ae65a2
Antonio F <hexfati@gmail.com>
2025-02-21 13:16:53 +0100

    included file header

--- wadas_webserver/mapper.py
+++ wadas_webserver/mapper.py
@@ -1,3 +1,21 @@
+# This file is part of WADAS project.
+#
+# WADAS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WADAS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WADAS. If not, see <https://www.gnu.org/licenses/>.
+#
+# Author(s): Stefano Dell'Osa, Alessandro Palla, Cesare Di Mauro, Antonio Farina
+# Date: 2025-02-21
+# Description: Class to map database-model objects to view-model objects.
 from wadas.domain.db_model import ActuationEvent as DB_ActuationEvent
 from wadas.domain.db_model import Actuator as DB_Actuator
 from wadas.domain.db_model import Camera as DB_Camera
@@ -15,6 +33,7 @@ from wadas_webserver.view_model import (
 
 
 class Mapper:
+    """Class to map database-model objects to view-model objects"""
 
     @staticmethod
     def map_db_user_to_user(db_user: DB_User) -> User:
--- wadas_webserver/server_config.py
+++ wadas_webserver/server_config.py
@@ -1,3 +1,21 @@
+# This file is part of WADAS project.
+#
+# WADAS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WADAS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WADAS. If not, see <https://www.gnu.org/licenses/>.
+#
+# Author(s): Stefano Dell'Osa, Alessandro Palla, Cesare Di Mauro, Antonio Farina
+# Date: 2025-02-21
+# Description: Class to handle WADAS Web Server configuration.
 import logging
 import secrets
 from pathlib import Path
@@ -8,6 +26,8 @@ logger = logging.getLogger(__name__)
 
 
 class ServerConfig:
+    """Class to handle WADAS Web Server configuration"""
+
     JWT_ENC_ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 15
     REFRESH_TOKEN_EXPIRE_DAYS = 7
