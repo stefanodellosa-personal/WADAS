537aea7170900c15d3ee5f5d24fe5cb188ffbf74
Stefano Dell'Osa <stefano.dellosa@gmail.com>
2025-03-02 16:46:36 +0100

    improved input format validation

--- wadas/ui/select_test_mode_input.py
+++ wadas/ui/select_test_mode_input.py
@@ -18,6 +18,7 @@
 # Description: Insert URL module
 
 import os
+import re
 
 import validators
 from PySide6.QtGui import QIcon
@@ -38,16 +39,19 @@ class DialogSelectTestModeInput(QDialog, Ui_DialogSelectTestModeInput):
         self.setWindowIcon(QIcon(os.path.join(module_dir_path, "..", "img", "mainwindow_icon.jpg")))
         self.ui.label_error.setStyleSheet("color: red")
         self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
+        self.ui.lineEdit_url.setText("https://www.parks.it/tmpFoto/30079_4_PNALM.jpeg") #TODO: remove for 1.0 release
         self.url = ""
         self.file_path = ""
 
         # Slots
-        self.ui.buttonBox.accepted.connect(self.accept_and_close)
+        self.ui.radioButton_url.clicked.connect(self.validate)
+        self.ui.radioButton_file.clicked.connect(self.validate)
         self.ui.radioButton_url.clicked.connect(self.on_input_radio_button_clicked)
         self.ui.radioButton_file.clicked.connect(self.on_input_radio_button_clicked)
         self.ui.lineEdit_url.textChanged.connect(self.validate)
         self.ui.lineEdit_file.textChanged.connect(self.validate)
         self.ui.pushButton_select_file.clicked.connect(self.on_select_file_button_clicked)
+        self.ui.buttonBox.accepted.connect(self.accept_and_close)
 
         self.on_input_radio_button_clicked()
 
@@ -63,7 +67,9 @@ class DialogSelectTestModeInput(QDialog, Ui_DialogSelectTestModeInput):
         """Method to select input from file"""
 
         input_file_type = "image" if self.ui.radioButton_image.isChecked() else "video"
-        input_file_extensions = "Jpeg (*.jpg), PNG (*.png)" if self.ui.radioButton_image.isChecked() else "mp4 (*.mp4)"
+        input_file_extensions = "JPEG (*.jpg, *.jpeg), PNG (*.png), BMP (*.bmp), TIFF (*.tiff, *.tif)" \
+            if self.ui.radioButton_image.isChecked() else\
+            "mp4 (*.mp4), AVI (*.AVI),  MOV (*.mov), MKV (*.mkv), WMV (*.wmv)"
 
         file_name = QFileDialog.getOpenFileName(
             self, f"Open {input_file_type} file", os.getcwd(), input_file_extensions
@@ -77,16 +83,38 @@ class DialogSelectTestModeInput(QDialog, Ui_DialogSelectTestModeInput):
         self.file_path = self.ui.lineEdit_file.text()
         self.accept()
 
+    def is_supported_media(self, url, is_image):
+        """Method to validate if given URL embeds correct file format"""
+
+        image_formats = (r'\.(jpg|jpeg|png|bmp|tiff|tif)$')
+        video_formats = (r'\.(mp4|avi|mov|mkv|wmv)$')
+
+        if is_image and re.search(image_formats, url, re.IGNORECASE):
+            return True
+        elif not is_image and re.search(video_formats, url, re.IGNORECASE):
+            return True
+        else:
+            return False
+
     def validate(self):
         """Method to validate provided input values"""
 
+        is_image = self.ui.radioButton_image.isChecked()
+        media_type_str = "image" if is_image else "video"
         if self.ui.radioButton_file.isChecked():
-            is_valid = os.path.isfile(self.ui.lineEdit_file.text())
+            url = self.ui.lineEdit_file.text()
+            is_valid = os.path.isfile(url)
             error_message = "" if is_valid else "Invalid file path provided"
         else:
             # Validate URL
-            is_valid = bool(validators.url(self.ui.lineEdit_url.text()))
+            url = self.ui.lineEdit_url.text()
+            is_valid = bool(validators.url(url))
             error_message = "" if is_valid else "Invalid URL provided"
+            if is_valid:
+                is_supported_format = self.is_supported_media(url, is_image)
+                error_message = "" if is_supported_format else f"Invalid {media_type_str} format at provided URL"
+                is_valid = is_supported_format
+
         self.ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(is_valid)
         self.ui.label_error.setText(error_message)
 
