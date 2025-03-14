203bd6f8e088af0ce6d489e5b0efca9b1ec88a19
Alessandro Palla <alespalla.ap@gmail.com>
2025-03-14 15:36:47 +0100

    Add licensing information and module description to object_tracker.py

--- wadas/ai/object_tracker.py
+++ wadas/ai/object_tracker.py
@@ -1,3 +1,22 @@
+# This file is part of WADAS project.
+#
+# WADAS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WADAS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WADAS. If not, see <https://www.gnu.org/licenses/>.
+#
+# Author(s): Stefano Dell'Osa, Alessandro Palla, Cesare Di Mauro, Antonio Farina
+# Date: 2025-03-14
+# Description: Module containing object tracker logic.
+
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
