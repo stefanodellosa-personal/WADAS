961fce38b344c2b53c6289f23dae6713380d94f0
Stefano Dell'Osa <stefano.dellosa@gmail.com>
2025-03-31 17:59:33 +0200

    improved configure tunnel ui

--- wadas/ui/qt/ui_configure_tunnel_mode.py
+++ wadas/ui/qt/ui_configure_tunnel_mode.py
@@ -15,54 +15,89 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractButton, QApplication, QButtonGroup, QComboBox,
-    QDialog, QDialogButtonBox, QGridLayout, QLabel,
-    QLayout, QPushButton, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QComboBox, QDialog,
+    QDialogButtonBox, QGridLayout, QLabel, QLayout,
+    QLineEdit, QPushButton, QSizePolicy, QWidget)
 
 class Ui_DialogConfigureTunnelMode(object):
     def setupUi(self, DialogConfigureTunnelMode):
         if not DialogConfigureTunnelMode.objectName():
             DialogConfigureTunnelMode.setObjectName(u"DialogConfigureTunnelMode")
-        DialogConfigureTunnelMode.resize(652, 418)
+        DialogConfigureTunnelMode.resize(652, 180)
         self.gridLayout = QGridLayout(DialogConfigureTunnelMode)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout_entrance_direction = QGridLayout()
         self.gridLayout_entrance_direction.setObjectName(u"gridLayout_entrance_direction")
         self.gridLayout_entrance_direction.setSizeConstraint(QLayout.SizeConstraint.SetFixedSize)
-        self.pushButton_new_tunnel = QPushButton(DialogConfigureTunnelMode)
-        self.pushButton_new_tunnel.setObjectName(u"pushButton_new_tunnel")
-
-        self.gridLayout_entrance_direction.addWidget(self.pushButton_new_tunnel, 0, 1, 1, 1)
-
-        self.label = QLabel(DialogConfigureTunnelMode)
-        self.label.setObjectName(u"label")
-
-        self.gridLayout_entrance_direction.addWidget(self.label, 0, 0, 1, 1)
-
         self.label_8 = QLabel(DialogConfigureTunnelMode)
         self.label_8.setObjectName(u"label_8")
 
         self.gridLayout_entrance_direction.addWidget(self.label_8, 2, 0, 1, 1)
 
-        self.comboBox_tunnel = QComboBox(DialogConfigureTunnelMode)
-        self.comboBox_tunnel.setObjectName(u"comboBox_tunnel")
+        self.comboBox_camera_2 = QComboBox(DialogConfigureTunnelMode)
+        self.comboBox_camera_2.setObjectName(u"comboBox_camera_2")
 
-        self.gridLayout_entrance_direction.addWidget(self.comboBox_tunnel, 1, 1, 1, 1)
+        self.gridLayout_entrance_direction.addWidget(self.comboBox_camera_2, 3, 1, 1, 1)
+
+        self.label_error = QLabel(DialogConfigureTunnelMode)
+        self.label_error.setObjectName(u"label_error")
+
+        self.gridLayout_entrance_direction.addWidget(self.label_error, 4, 0, 1, 2)
 
         self.label_7 = QLabel(DialogConfigureTunnelMode)
         self.label_7.setObjectName(u"label_7")
 
         self.gridLayout_entrance_direction.addWidget(self.label_7, 1, 0, 1, 1)
 
-        self.comboBox_camera = QComboBox(DialogConfigureTunnelMode)
-        self.comboBox_camera.setObjectName(u"comboBox_camera")
+        self.pushButton_add_new_tunnel = QPushButton(DialogConfigureTunnelMode)
+        self.pushButton_add_new_tunnel.setObjectName(u"pushButton_add_new_tunnel")
 
-        self.gridLayout_entrance_direction.addWidget(self.comboBox_camera, 2, 1, 1, 1)
+        self.gridLayout_entrance_direction.addWidget(self.pushButton_add_new_tunnel, 0, 1, 1, 1)
 
-        self.label_2 = QLabel(DialogConfigureTunnelMode)
-        self.label_2.setObjectName(u"label_2")
+        self.lineEdit_new_tunnel_name = QLineEdit(DialogConfigureTunnelMode)
+        self.lineEdit_new_tunnel_name.setObjectName(u"lineEdit_new_tunnel_name")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEdit_new_tunnel_name.sizePolicy().hasHeightForWidth())
+        self.lineEdit_new_tunnel_name.setSizePolicy(sizePolicy)
 
-        self.gridLayout_entrance_direction.addWidget(self.label_2, 3, 0, 1, 2)
+        self.gridLayout_entrance_direction.addWidget(self.lineEdit_new_tunnel_name, 0, 0, 1, 1)
+
+        self.label = QLabel(DialogConfigureTunnelMode)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_entrance_direction.addWidget(self.label, 3, 0, 1, 1)
+
+        self.comboBox_tunnel = QComboBox(DialogConfigureTunnelMode)
+        self.comboBox_tunnel.setObjectName(u"comboBox_tunnel")
+
+        self.gridLayout_entrance_direction.addWidget(self.comboBox_tunnel, 1, 1, 1, 1)
+
+        self.comboBox_camera_1 = QComboBox(DialogConfigureTunnelMode)
+        self.comboBox_camera_1.setObjectName(u"comboBox_camera_1")
+
+        self.gridLayout_entrance_direction.addWidget(self.comboBox_camera_1, 2, 1, 1, 1)
+
+        self.pushButton_direction_camera2 = QPushButton(DialogConfigureTunnelMode)
+        self.pushButton_direction_camera2.setObjectName(u"pushButton_direction_camera2")
+
+        self.gridLayout_entrance_direction.addWidget(self.pushButton_direction_camera2, 3, 3, 1, 1)
+
+        self.label_direction_camera_2 = QLabel(DialogConfigureTunnelMode)
+        self.label_direction_camera_2.setObjectName(u"label_direction_camera_2")
+
+        self.gridLayout_entrance_direction.addWidget(self.label_direction_camera_2, 3, 2, 1, 1)
+
+        self.label_direction_camera_1 = QLabel(DialogConfigureTunnelMode)
+        self.label_direction_camera_1.setObjectName(u"label_direction_camera_1")
+
+        self.gridLayout_entrance_direction.addWidget(self.label_direction_camera_1, 2, 2, 1, 1)
+
+        self.pushButton_direction_camera_1 = QPushButton(DialogConfigureTunnelMode)
+        self.pushButton_direction_camera_1.setObjectName(u"pushButton_direction_camera_1")
+
+        self.gridLayout_entrance_direction.addWidget(self.pushButton_direction_camera_1, 2, 3, 1, 1)
 
 
         self.gridLayout.addLayout(self.gridLayout_entrance_direction, 0, 0, 1, 1)
@@ -84,10 +119,14 @@ class Ui_DialogConfigureTunnelMode(object):
 
     def retranslateUi(self, DialogConfigureTunnelMode):
         DialogConfigureTunnelMode.setWindowTitle(QCoreApplication.translate("DialogConfigureTunnelMode", u"Configure Tunnel Mode", None))
-        self.pushButton_new_tunnel.setText(QCoreApplication.translate("DialogConfigureTunnelMode", u"new tunnel", None))
-        self.label.setText(QCoreApplication.translate("DialogConfigureTunnelMode", u"TextLabel", None))
-        self.label_8.setText(QCoreApplication.translate("DialogConfigureTunnelMode", u"Select camera", None))
+        self.label_8.setText(QCoreApplication.translate("DialogConfigureTunnelMode", u"Select camera for tunnel entrance 1", None))
+        self.label_error.setText("")
         self.label_7.setText(QCoreApplication.translate("DialogConfigureTunnelMode", u"Select tunnel", None))
-        self.label_2.setText("")
+        self.pushButton_add_new_tunnel.setText(QCoreApplication.translate("DialogConfigureTunnelMode", u"add new tunnel", None))
+        self.label.setText(QCoreApplication.translate("DialogConfigureTunnelMode", u"Select camera for tunnel entrance 2", None))
+        self.pushButton_direction_camera2.setText(QCoreApplication.translate("DialogConfigureTunnelMode", u"select entrance direction", None))
+        self.label_direction_camera_2.setText("")
+        self.label_direction_camera_1.setText("")
+        self.pushButton_direction_camera_1.setText(QCoreApplication.translate("DialogConfigureTunnelMode", u"select entrance direction", None))
     # retranslateUi
 
